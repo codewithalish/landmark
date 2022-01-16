410f0f77d42110c3385b86b79b223fbbfed05ef6
fateme mozafari <fatemem1990@yahoo.com>
2022-01-16 09:01:11 +0330

    create landmark masterpage

---
+++ resources/views/layouts/partials/landmark/head.blade.php
@@ -0,0 +1,24 @@
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0">
+    <link rel="shortcut icon" href="/landmark/images/favicon.ico">
+    <title>قالب مشاور املاک LandMark</title>
+
+    <link rel="stylesheet" href="/landmark/css/bootstrap.min.css" type="text/css" media="all">
+    <link rel="stylesheet" href="/landmark/css/font-awesome.min.css" type="text/css" media="all">
+    <link rel="stylesheet" href="/landmark/css/ionicons.min.css" type="text/css" media="all">
+    <link rel="stylesheet" href="/landmark/css/icon-moon.css" type="text/css" media="all">
+    <link rel="stylesheet" href="/landmark/css/magnific-popup.min.css">
+    <link rel="stylesheet" href="/landmark/css/animate.css">
+    <link rel="stylesheet" href="/landmark/css/owl.carousel.min.css" type="text/css" media="all">
+    <link rel="stylesheet" href="/landmark/css/owl.theme.default.min.css" type="text/css" media="all">
+    <link rel="stylesheet" href="/landmark/css/style.css" type="text/css" media="all">
+    <link rel="stylesheet" href="/landmark/css/custom.css" type="text/css" media="all">
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+    <script src="/landmark/js/html5shiv.min.js"></script>
+    <script src="/landmark/js/respond.min.js"></script>
+    <![endif]-->
+</head>
