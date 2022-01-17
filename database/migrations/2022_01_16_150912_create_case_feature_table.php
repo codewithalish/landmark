cb9ce9cd788670d2acd1f80a5a891216166d5377
codewithalish <95971927+codewithalish@users.noreply.github.com>
2022-01-17 19:44:17 +0330

    fix bugs on migration

--- database/migrations/2022_01_16_150912_create_case_feature_table.php
+++ database/migrations/2022_01_16_150912_create_case_feature_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateCaseFeaturesTable extends Migration
+class CreateCaseFeatureTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,7 +13,7 @@ class CreateCaseFeaturesTable extends Migration
      */
     public function up()
     {
-        Schema::create('case_features', function (Blueprint $table) {
+        Schema::create('case_feature', function (Blueprint $table) {
             $table->id();
             $table->integer('case_id');
             $table->integer('feature_id');
