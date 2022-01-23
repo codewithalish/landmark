1f81ab6cf5b106c7c75e7457a7483c115206e7e7
fateme mozafari <fatemem1990@yahoo.com>
2022-01-23 13:16:46 +0330

    work on agent files

---
+++ app/Http/Controllers/AgentController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Agent;
+use App\Models\CaseModel;
+use App\Models\Post;
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class AgentController extends Controller
+{
+    public function allAgents(Request $request)
+    {
+
+        $items=User::query()
+            ->orderBy('created_at', 'desc')
+            ->get();
+
+        return view('pages/agent_list', compact('items'));
+    }
+
+//    public function index()
+//    {
+//        //
+//        $query=Post::get();
+//        return view('afracode.posts.index',['items'=>$query]);
+//    }
+
+
+    public function show($id)
+    {
+        $query=User::find($id);
+        return view('pages/agent_detail',['item'=>$query]);
+    }
+
+
+}
