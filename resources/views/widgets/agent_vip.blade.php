d26b6b9b4574b8c5d7a6e1e94c275afec8ae7a67
fateme mozafari <fatemem1990@yahoo.com>
2022-01-23 13:17:44 +0330

    add widgets

---
+++ resources/views/widgets/agent_vip.blade.php
@@ -0,0 +1,44 @@
+<div class="widget">
+    <h3 class="widget-title">نمایندگان ویژه</h3>
+    <div class="featured-agent">
+        <div class="agent-item">
+            <div class="thumbnail">
+                <a href="agent-detail.html" title="Clark Darik">
+                    <img src="/landmark/images/team/thumb/agent_1.png" alt="Clark Darik">
+                </a>
+            </div>
+            <div class="content">
+                <div class="item-title">
+                    <a href="agent-detail.html" title="Clark Darik">کلارک کنت </a>
+                </div>
+                <span class="total-property">0 ملک </span>
+            </div>
+        </div>
+        <div class="agent-item">
+            <div class="thumbnail">
+                <a href="agent-detail.html" title="Sarah Barabova">
+                    <img src="/landmark/images/team/thumb/agent_2.png" alt="Sarah Barabova">
+                </a>
+            </div>
+            <div class="content">
+                <div class="item-title">
+                    <a href="agent-detail.html" title="Sarah Barabova">سارا لنس </a>
+                </div>
+                <span class="total-property">0 ملک </span>
+            </div>
+        </div>
+        <div class="agent-item">
+            <div class="thumbnail">
+                <a href="agent-detail.html" title="Bruck Banner">
+                    <img src="/landmark/images/team/thumb/agent_3.png" alt="Bruck Banner">
+                </a>
+            </div>
+            <div class="content">
+                <div class="item-title">
+                    <a href="agent-detail.html" title="Bruck Banner">بروس بنر </a>
+                </div>
+                <span class="total-property">0 ملک </span>
+            </div>
+        </div>
+    </div>
+</div>
---
+++ resources/views/widgets/last_cases.blade.php
@@ -0,0 +1,56 @@
+<div class="widget">
+    <h3 class="widget-title">ملک های اخیر</h3>
+    <div class="recent-property-wrap">
+        <div class="recent-property-item">
+            <div class="thumbnail">
+                <a href="property-fullwidth.html" title="Family House in Hudson">
+                    <img src="/landmark/images/property/thumb/property_1.jpg" alt="Family House in Hudson">
+                </a>
+            </div>
+            <div class="content">
+                <div class="item-title">
+                    <a href="property-fullwidth.html" title="Family House in Hudson">
+                        لورم ایپسوم متن ساختگی
+                    </a>
+                </div>
+                <div class="price">
+                    <span class="amount">560,000,000 تومان</span>
+                </div>
+            </div>
+        </div>
+        <div class="recent-property-item">
+            <div class="thumbnail">
+                <a href="property-fullwidth.html" title="Loft Above The City">
+                    <img src="/landmark/images/property/thumb/property_2.jpg" alt="Loft Above The City">
+                </a>
+            </div>
+            <div class="content">
+                <div class="item-title">
+                    <a href="property-fullwidth.html" title="Loft Above The City">
+                        لورم ایپسوم متن ساختگی
+                    </a>
+                </div>
+                <div class="price">
+                    <span class="amount">560,000,000 تومان</span>
+                </div>
+            </div>
+        </div>
+        <div class="recent-property-item">
+            <div class="thumbnail">
+                <a href="property-fullwidth.html" title="Villa on Hollywood Boulevard">
+                    <img src="/landmark/images/property/thumb/property_3.jpg" alt="Villa on Hollywood Boulevard">
+                </a>
+            </div>
+            <div class="content">
+                <div class="item-title">
+                    <a href="property-fullwidth.html" title="Villa on Hollywood Boulevard">
+                        لورم ایپسوم متن ساختگی با تولید
+                    </a>
+                </div>
+                <div class="price">
+                    <span class="amount">560,000,000 تومان</span>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
