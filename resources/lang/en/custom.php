300334f9dc88a4e8d82ed6fc22432181396aecbf
fateme <fatemem1990@yahoo.com>
2022-01-27 13:26:38 +0330

    add custom lang

---
+++ resources/lang/en/custom.php
@@ -0,0 +1,11 @@
+<?php
+return [
+
+    "contract"=>[
+        'FOR_SALE'=>'برای فروش',
+        'FOR_RENT'=>'برای اجاره'
+    ]
+];
+
+
+?>
---
+++ resources/lang/fa/custom.php
@@ -0,0 +1,11 @@
+<?php
+return [
+
+"contracts"=>[
+    'FOR_SALE'=>'برای فروش',
+    'FOR_RENT'=>'برای اجاره'
+]
+
+
+];
+?>
