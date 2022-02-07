702e91b8d92d1e32ca4a27b9e9d077956162230e
majid salehi <majidsalehi67@gmail.com>
2022-02-07 12:11:37 +0330

    change some fields

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -17,13 +17,13 @@
                         <div class="card-group d-block d-md-flex row">
                             <div class="card col-md-7 p-4 mb-0">
                                 <div class="card-body">
-                                    <h1>Login</h1>
-                                    <p class="text-medium-emphasis">Sign In to your account</p>
+                                    <h1>ورود</h1>
+                                    <p class="text-medium-emphasis">وارد پروفایل خود شوید</p>
                                     <div class="input-group mb-3"><span class="input-group-text">
                       <svg class="icon">
                         <use xlink:href="/admin/login/node_modules/@coreui/icons/sprites/free.svg#cil-user"></use>
                       </svg></span>
-                                        <input class="form-control" type="text" placeholder="Username" name="email">
+                                        <input class="form-control" type="text" placeholder="نام کاربری" name="mobile">
                                     </div>
                                     {{--                                @error('password')--}}
                                     {{--                                <span style="color: red">{{$message}}</span>--}}
@@ -32,17 +32,17 @@
                       <svg class="icon">
                         <use xlink:href="/node_modules/@coreui/icons/sprites/free.svg#cil-lock-locked"></use>
                       </svg></span>
-                                        <input class="form-control" type="password" placeholder="Password" name="password">
+                                        <input class="form-control" type="password" placeholder="کلمه عبور" name="password">
                                     </div>
                                     {{--                                @error('password')--}}
                                     {{--                                <span style="color: red">{{$message}}</span>--}}
                                     {{--                                @enderror--}}
                                     <div class="row">
                                         <div class="col-6">
-                                            <button class="btn btn-primary px-4" type="submit">Login</button>
+                                            <button class="btn btn-primary px-4" type="submit">ورود</button>
                                         </div>
                                         <div class="col-6 text-end">
-                                            <button class="btn btn-link px-0" type="button">Forgot password?</button>
+                                            <button class="btn btn-link px-0" type="button">کله عبور خود را فراموش کردید؟</button>
                                         </div>
                                     </div>
                                 </div>
@@ -50,10 +50,10 @@
                             <div class="card col-md-5 text-white bg-primary py-5">
                                 <div class="card-body text-center">
                                     <div>
-                                        <h2>Sign up</h2>
+                                        <h2>ثبت نام</h2>
                                         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
                                         <a href="/register">
-                                            <button class="btn btn-lg btn-outline-light mt-3" type="button">Register Now!</button>
+                                            <button class="btn btn-lg btn-outline-light mt-3" type="button">همین الان ثبت نام کنید</button>
                                         </a>
                                     </div>
                                 </div>
