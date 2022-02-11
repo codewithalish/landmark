96ccbc792c2e81680cd064aec842fa6f78d9dc02
majid salehi <majidsalehi67@gmail.com>
2022-02-11 13:28:38 +0330

    add edit in category & fix some bugs

---
+++ resources/views/admin/categories/edit.blade.php
@@ -0,0 +1,27 @@
+@extends('layouts.masterPages.admin')
+@section('content')
+    <div class="body flex-grow-1 px-3">
+        <div class="container-lg">
+
+            {{--            <div class="callout callout-info bg-white">اطلاعات پیج</div>--}}
+            <div class="car"></div>
+            <div class="card mb-4">
+                <div class="card-header">{{$cardTitle ?? ''}}</div>
+                @include('layouts.partials.auth.alert')
+
+                <form action="/admin/categories/{{$item->id}}" method="post">
+                    @csrf
+                    @method('put')
+
+                        <div class="mb-3">
+                            <label class="form-label" for="title">عنوان</label>
+                            <input class="form-control" id="title" type="text" placeholder="title" name="title" value="{{$item->title}}">
+                        </div>
+                        <button type="submit" class="btn btn-primary active">ویرایش</button>
+                    </div>
+                </form>
+
+            </div>
+        </div>
+    </div>
+@endsection
---
+++ resources/views/admin/categories/show.blade.php
@@ -0,0 +1,19 @@
+@extends('layouts.masterPages.admin')
+@section('content')
+    <div class="body flex-grow-1 px-3">
+        <div class="container-lg">
+            <div class="callout callout-info bg-white">اطلاعات پست
+            </div>
+            <div class="car"></div>
+            <div class="card mb-4">
+                <div class="card-header">{{$titleCard ?? ''}}</div>
+                <div class="card-body">
+                    <img src="{{$item->title }}" alt="{{$item->title}}">
+                    <hr>
+                    <strong>title:</strong>
+                    <span>{{$item->title}}</span>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
