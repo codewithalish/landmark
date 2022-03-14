709316786cf49a0b33d9d59076d37e3b45df983d
fateme mozafari <fatemem1990@yahoo.com>
2022-03-14 10:14:51 +0330

    add newsletterBody table,model,controller

---
+++ app/Http/Controllers/admin/NewsletterBodyController.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Http\Controllers\admin;
+
+use App\Http\Controllers\Controller;
+use App\Models\Newsletter;
+use App\Models\NewsletterBody;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class NewsletterBodyController extends Controller
+{
+    public function create()
+    {
+        $titleCard = 'لیست';
+        $th = ['شناسه', 'body', 'operation'];
+        $query = NewsletterBody::query()
+            ->orderBy('id', 'DESC')
+            ->limit(10)
+            ->get();
+        return view('admin.newsletters.create',
+            [
+                'items' => $query,
+                'th' => $th,
+                'titleCard' => $titleCard,
+            ]
+        );
+//        return view('admin.newsletters.create');
+    }
+
+    public function store(Request $request)
+    {
+        $inputs = $request->only(
+            'body'
+        );
+        $inputs['user_id'] = Auth::user()->id;
+
+        $result = NewsletterBody::create($inputs);
+
+        if ($result) {
+            return back()->with('success', 'با موفقیت ارسال شد');
+        } else {
+            return back()->with('error');
+        }
+
+    }
+    public function destroy($id)
+    {
+        $del=NewsletterBody::query()->where('id', $id)->delete();
+        return view('admin.newsletters.create',compact('del'));
+    }
+
+}
