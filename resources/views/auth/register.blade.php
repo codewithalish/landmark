bb181d2772a4115198a865c9fed7c76765745495
fateme <fatemem1990@yahoo.com>
2022-04-23 14:14:23 +0430

    edit Register blade

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -9,60 +9,70 @@
                 <div class="col-lg-8">
                     @include('layouts.partials.auth.alert')
 
-                    <form action="/register" method="post">
+                    <form action="/admins/register" method="post">
                         @csrf
                         <div class="card-group d-block d-md-flex row">
                             <div class="card col-md-7 p-4 mb-0">
 
-                                    <div class="card-body">
-                                        <h1>ثبت نام</h1>
-                                        <p class="text-medium-emphasis">پروفایل خود را ثبت کنید</p>
-                                        <div class="input-group mb-3"><span class="input-group-text">
+                                <div class="card-body">
+                                    <h1>ثبت نام</h1>
+                                    <p class="text-medium-emphasis">پروفایل خود را ثبت کنید</p>
+                                    <div class="input-group mb-3"><span class="input-group-text">
                       <svg class="icon">
                         <use xlink:href="/admin/login/node_modules/@coreui/icons/sprites/free.svg#cil-user"></use>
                       </svg></span>
-                                            <input class="form-control" type="text" placeholder="نام" name="name">
-                                        </div>
-                                        <div class="input-group mb-3"><span class="input-group-text">
-                      <svg class="icon">
-                        <use xlink:href="/admin/login/node_modules/@coreui/icons/sprites/free.svg#cil-user"></use>
-                      </svg></span>
-                                            <input class="form-control" type="text" placeholder="تلفن همراه" name="mobile">
-                                        </div>
-                                        {{--                                    @error('email')--}}
-                                        {{--                                    <span style="color: red">{{$message}}</span>--}}
-                                        {{--                                    @enderror--}}
-                                        <div class="input-group mb-4"><span class="input-group-text">
-                      <svg class="icon">
-                        <use xlink:href="/admin/login/node_modules/@coreui/icons/sprites/free.svg#cil-lock-locked"></use>
-                      </svg></span>
-                                            <input class="form-control" type="password" placeholder="کلمه عبور" name="password">
-                                        </div>
-                                        {{--                                    @error('password')--}}
-                                        {{--                                    <span style="color: red">{{$message}}</span>--}}
-                                        {{--                                    @enderror--}}
-                                        <div class="row">
-                                            <div class="col-6">
-                                                <button class="btn btn-primary px-4" type="submit">ثبت نام</button>
-                                            </div>
-
-                                        </div>
+                                        <input class="form-control" type="text" placeholder="نام" name="name">
                                     </div>
+                                    <div class="input-group mb-3"><span class="input-group-text">
+                      <svg class="icon">
+                        <use xlink:href="/admin/login/node_modules/@coreui/icons/sprites/free.svg#cil-user"></use>
+                      </svg></span>
+                                        <input class="form-control" type="text" placeholder="تلفن همراه" name="mobile">
+                                    </div>
+                                    {{--                                    @error('email')--}}
+                                    {{--                                    <span style="color: red">{{$message}}</span>--}}
+                                    {{--                                    @enderror--}}
+                                    <div class="input-group mb-4"><span class="input-group-text">
+                                                  <svg class="icon">
+                        <use xlink:href="/admin/login/node_modules/@coreui/icons/sprites/free.svg#cil-user"></use>
+                      </svg></span>
+                                        <input class="form-control" type="text" placeholder="ایمیل" name="email">
+                                    </div>
+                                    <div class="input-group mb-3"><span class="input-group-text">
+                      <svg class="icon">
+                        <use
+                            xlink:href="/admin/login/node_modules/@coreui/icons/sprites/free.svg#cil-lock-locked"></use>
+                      </svg></span>
+                                        <input class="form-control" type="password" placeholder="کلمه عبور"
+                                               name="password">
+                                    </div>
+                                    {{--                                    @error('password')--}}
+                                    {{--                                    <span style="color: red">{{$message}}</span>--}}
+                                    {{--                                    @enderror--}}
+                                    <div class="row">
+                                        <div class="col-6">
+                                            <button class="btn btn-primary px-4" type="submit">ثبت نام</button>
+                                        </div>
+
+                                    </div>
+                                </div>
 
 
                             </div>
                             <div class="card col-md-5 text-white bg-primary py-5">
 
-                                    <div class="card-body text-center">
+                                <div class="card-body text-center">
 
-                                        <div>
-                                            <h2>ثبت نام</h2>
-                                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-                                            <a href="/login">
-                                                <button class="btn btn-lg btn-outline-light mt-3" type="button">وارد شوید</button>
-                                            </a>
-                                        </div>
+                                    <div>
+                                        <h2>ثبت نام</h2>
+                                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                                            tempor incididunt ut labore et dolore magna aliqua.</p>
+                                        <a href="/admins/login">
+                                            <button class="btn btn-lg btn-outline-light mt-3" type="button">وارد شوید
+                                            </button>
+                                        </a>
                                     </div>
+                                </div>
 
 
                             </div>
